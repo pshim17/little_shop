eeba8fe5dd4b38feb57087942f3a779796dbebb2
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-01-08 13:28:23 -0800

    feat:updated spec. Adding sad path test

--- spec/requests/api/v1/merchants/create_merchants_spec.rb
+++ spec/requests/api/v1/merchants/create_merchants_spec.rb
@@ -11,4 +11,14 @@ RSpec.describe "Create a Merchant", type: :request do
     merchant = JSON.parse(response.body, symbolize_names: true)[:data]
     expect(merchant[:attributes][:name]).to eq('Amazon')
   end
+  it "returns an error if the name is missing" do
+    invalid_params = { name: "" }
+
+    post "/api/v1/merchants", params: invalid_params
+
+    expect(response).not_to be_successful
+    expect(response.status).to eq(422)
+    error = JSON.parse(response.body, symbolize_names: true)
+    expect(error[:error]).to eq("unprocessable entity")
+  end
 end
