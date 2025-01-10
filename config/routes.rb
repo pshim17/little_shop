a5502021c58e44fe8bfd206c2ea2c2a5ee5054a8
Jonathan Atkins <jonathanjatkins89@gmail.com>
2025-01-09 18:22:39 -0800

    updating to merge with main

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get "/api/v1/merchants", to: "api/v1/merchants#index"
   post "/api/v1/merchants", to: "api/v1/merchants#create" 
   patch "/api/v1/merchants/:id", to: "api/v1/merchants#update"
+  delete "/api/v1/merchants/:id", to: "api/v1/merchants#destroy"
 
   # Items
   get "/api/v1/items", to: "api/v1/items#index"
