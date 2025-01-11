13daa21c82931af0da7c7664aba2a5ea5b6803b6
Kevin Newland <kevin.newland@yahoo.com>
2025-01-11 16:25:58 -0600

    add def show for finding merchant by id

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -12,6 +12,11 @@ class Api::V1::MerchantsController < ApplicationController
    render json: MerchantSerializer.new(merchants, { params: { count: params[:count] } })
   end
 
+  def show
+    merchant = Merchant.find(params[:id])
+    render json: MerchantSerializer.new(merchant)
+  end
+
   def create
     begin
       merchant = Merchant.create!(merchant_params)
