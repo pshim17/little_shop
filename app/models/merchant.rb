fa3dca933de19702d81f419e3db99c2034f9469b
Kevin Newland <kevin.newland@yahoo.com>
2025-01-12 14:41:20 -0600

    add methods to handle database logic

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -7,4 +7,16 @@ class Merchant < ApplicationRecord
     # require'pry';binding.pry
     where("name LIKE ?", "%#{name}%").limit(1).first
   end
+
+  def self.sort_by_age
+    order(created_at: :desc)
+  end
+
+  def self.returned_invoices
+    joins(:invoices).where(invoices: {status: "returned"}).distinct
+  end
+
+  def self.add_item_count
+    left_joins(:items).select("merchants.*, COUNT(items.id) AS item_count").group("merchants.id").order(id: :asc)
+  end
 end
