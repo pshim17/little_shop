3b153463f8a9db214d3446e2f1fa5b0c5d17536a
Kristin Weiland <kristinweiland@gmail.com>
2025-01-12 12:20:22 -0800

    Add more expect statements to the customers_by_merchant testing.

--- spec/customers_controller_spec.rb
+++ spec/customers_controller_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe "Little Shop API", type: :request do    
 
-  it "shows customers" do
+  it "shows customer names by merchant" do
+   
     test_merchant1 = Merchant.create!(name: "Test Merchant1")
     test_merchant2 = Merchant.create!(name: "Test Merchant2")
 
@@ -18,28 +19,35 @@ describe "Little Shop API", type: :request do
       "first_name": "Albina",
       "last_name": "Erdman"
     })
+    customer4 = Customer.create!( {
+      "first_name": "Karol",
+      "last_name": "Eloiwan"
+    })
 
     invoice1 = Invoice.create!(customer_id: customer1.id, merchant_id: test_merchant1.id, status: "shipped")
-    invoice2 = Invoice.create!(customer_id: customer1.id, merchant_id: test_merchant1.id, status: "shipped")
-
-    
-    # Create an initial item to test updates
-    test_item_1 = Item.create!(
-    name: "Old Item Name",
-    description: "Old description of the item.",
-    unit_price: 50.00,
-    merchant_id: test_merchant.id
-  )
+    invoice2 = Invoice.create!(customer_id: customer2.id, merchant_id: test_merchant1.id, status: "shipped")
+    invoice3 = Invoice.create!(customer_id: customer3.id, merchant_id: test_merchant1.id, status: "shipped")
+    invoice4 = Invoice.create!(customer_id: customer4.id, merchant_id: test_merchant2.id, status: "shipped")
   
-    get "/api/v1/customers", params: 
+    get "/api/v1/customers/merchant/#{test_merchant1.id}" 
 
-    # Reload the item to reflect updated changes
-    test_item_1.reload
-
-    # Expect the response to return the correct updated attributes
     expect(response).to be_successful
-    expect(test_item_1.name).to eq(updated_attributes[:name])
-    expect(test_item_1.description).to eq(updated_attributes[:description])
-    expect(test_item_1.unit_price).to eq(updated_attributes[:unit_price])
+    expect(response).to be_an(Array)
+
+    customers = JSON.parse(response.body, symbolize_names: true)
+
+    expect(customers.size).to eq(3)
+
+    expect(customers[:data][0][:attributes][:first_name]).to eq(customer1.first_name)
+    expect(customers[:data][0][:attributes][:last_name]).to eq(customer1.last_name)
+
+    customers.each do |customer|
+      expect(customer).to have_key(first_name)
+      expect(customer[:first_name].to be_a(String)
+
+      expect(customer).to have_key("last_name")
+      expect(customer["last_name"]).to be_a(String)
+    end
+    
   end
 end
